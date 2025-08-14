8a20219c46037a6a1d350f047517eeb422aef6bd
ingefaerel <elmiramust2010@gmail.com>
2025-08-14 22:41:06 +0200

    some

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// Routes mounted to match existing frontend usage
+// Routes mounted to match existing frontend usage gjhghjgjhgjhghj
 app.use("/tasks/note", notesRouter); // mount this first
 app.use("/tasks", tasksRouter);
 app.use("/api/calendar", calendarRouter);
